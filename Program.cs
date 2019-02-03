d83279cd10216c2be279fabeacbb800f29859362
isaevyaroslav <iyaroslav@yandex.ru>
2019-02-04 00:09:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLightModul5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
